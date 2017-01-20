96110497ffebc6e1f48993f987bec7859a0994b4
wwnje <939685623@qq.com>
2017-01-20 16:53:51 +0800

    none

--- unity/AS/Xfyun/app/src/main/java/com/example/xfyun/MainActivity.java
+++ unity/AS/Xfyun/app/src/main/java/com/example/xfyun/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends UnityPlayerActivity {
     private static String TAG = "MainActivity";
     public SpeechRecognizer mspeech;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
